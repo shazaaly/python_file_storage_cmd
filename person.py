8d049a639746ab482ced83c3ac3f7685b5a67797
shazaaly <shaza.aly@gmail.com>
2023-08-05 10:09:50 +0300

    porson.json

--- person.py
+++ person.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import json
+
 
 class Person:
 
@@ -9,3 +11,13 @@ class Person:
 
     def __str__(self) -> str:
         print(f"Name : {self.name} - Age : {self.age}")
+
+
+person_1 = Person("Ahmed", 27)
+person1_dict = person_1.__dict__
+print(person1_dict)
+person_1_json = json.dumps(person1_dict)
+print(person_1_json)
+
+with open("person.json", "w") as file:
+    file.write(person_1_json)
