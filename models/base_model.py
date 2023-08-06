003b9cefe7d2e22f8163b0596f7e09e4415e85ab
shazaaly <shaza.aly@gmail.com>
2023-08-06 16:36:46 +0300

    isoformat

--- models/base_model.py
+++ models/base_model.py
@@ -38,6 +38,8 @@ class BaseModel:
         """ returns a dictionary of keys/values of __dict__ of the instance:
                 """
 
-        obj_dict = self.__dict__
+        obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
