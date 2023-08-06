0fe0668c44c8d9e8817f65700813bba40315ec78
shazaaly <shaza.aly@gmail.com>
2023-08-06 22:18:38 +0300

    kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,25 @@ class BaseModel:
     """A class to be used as base class to rest of models
     """
 
-    def __init__(self, id=None, created_at=None, updated_at=None) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         """Constructor to initialize Public instance attributes in need at base
                 Args:
                         id (_type_): uuidv4
                         created_at (_type_): datetime
                         updated_at (_type_): datetime
         """
-        self.id = id or str(uuid.uuid4())
-        self.created_at = created_at or datetime.now()
-        self.updated_at = updated_at or datetime.now()
+        if kwargs and len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == " __class__ ":
+                    continue
+                elif key in ["created_at", "updated_at"]:
+                    value = value.isoformat()
+                setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self) -> str:
         """function returns string representatoin of BaseModel objects
