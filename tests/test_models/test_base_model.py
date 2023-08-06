a15c4853341cdf581b2cf934713c7f21848ae61c
shazaaly <shaza.aly@gmail.com>
2023-08-06 13:18:04 +0300

    test_base_model file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is the unittest of Base Module
+It contains a class for unitest
+"""
+
+import unittest
+import os
+
+from models.base_model import BaseModel
+
+
+class TestBaseInstantation(unittest.TestCase):
+    pass
